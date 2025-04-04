980af53ddc01b75ef0e4c46b714887d5a07ef457
maximedeboeck <119764381+maximedeboeck@users.noreply.github.com>
2025-04-04 20:38:23 +0200

    bit of cleanup

--- solver/src/main.c
+++ solver/src/main.c
@@ -7,39 +7,16 @@ int main() {
   problem *theProblem;
   geo *theGeometry;
   double *theSoluce =
-      compute_solution("../data/mensh.txt", &theProblem, &theGeometry, 0, NULL, 0);
+      compute_solution("../data/mesh.txt", &theProblem, &theGeometry, 0, NULL, 0);
 
   nodes *theNodes = theGeometry->theNodes;
   double deformationFactor = 1e5;
   double *normDisplacement = malloc(theNodes->nNodes * sizeof(double));
-  double *forcesX = malloc(theNodes->nNodes * sizeof(double));
-  double *forcesY = malloc(theNodes->nNodes * sizeof(double));
 
   for (int i = 0; i < theNodes->nNodes; i++) {
     theNodes->X[i] += theSoluce[2 * i + 0] * deformationFactor;
     theNodes->Y[i] += theSoluce[2 * i + 1] * deformationFactor;
     normDisplacement[i] = sqrt(theSoluce[2 * i + 0] * theSoluce[2 * i + 0] +
                                theSoluce[2 * i + 1] * theSoluce[2 * i + 1]);
-    // forcesX[i] = theForces[2*i+0];
-    // forcesY[i] = theForces[2*i+1];
   }
-  FILE *file = fopen("solution.txt", "w");
-
-  for (int i = 0; i < theProblem->geometry->theNodes->nNodes; i++) {
-
-    // 10 digit precsion
-
-    fprintf(file, "%14.7e %14.7e %d\n", theSoluce[2 * i + 0],
-            theSoluce[2 * i + 1], i);
-  }
-
-  fclose(file);
-  // double theGlobalForce[2] = {0, 0};
-  // for (int i=0; i<theProblem->geometry->theNodes->nNodes; i++) {
-  //     theGlobalForce[0] += theForces[2*i+0];
-  //     theGlobalForce[1] += theForces[2*i+1]; }
-  // printf(" ==== Global horizontal force       : %14.7e [N]
-  // \n",theGlobalForce[0]); printf(" ==== Global vertical force         :
-  // %14.7e [N] \n",theGlobalForce[1]); printf(" ==== Weight : %14.7e [N] \n",
-  // area * rho * g);
 }
